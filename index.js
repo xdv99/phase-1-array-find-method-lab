4e8c7d24ecc64750e29833a3fa8dbfdf3cd2d563
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-24 22:46:27 +0300

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin (record) {
+    const winRecord = record.find(element => element.result === 'W');
+    if (typeof winRecord === 'object') {
+        return winRecord.year;
+    } else {
+        return undefined;
+    }
+}
